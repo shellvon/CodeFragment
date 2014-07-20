2ad242675cbb43a6021fa636ba98d9bcc4fe734d
shellvon <iamshellvon@gmail.com>
2014-07-20 23:45:45 +0800

    delete unless files

--- googletrans_chrome_extensions/js/fuck.js
+++
@@ -1,8 +0,0 @@
-/**...not work in chrome://(Not support this scheme..fuck...it's bug for me.)**/
-window.onmouseup = function(){
-	/*console.log('select sth');*/
-    var selection = window.getSelection();
-    if(selection.anchorOffset != selection.extentOffset){
-        chrome.runtime.sendMessage(selection.toString().trim());
-    }
-}
